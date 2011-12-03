c4ff8eb38e28268df9fc949a6f6e7bfc739cd057
Daniel Grech <danielgrech91@gmail.com>
2011-12-03 15:43:20 +1100

    Added mention auto-completion

--- src/com/DGSD/TweeterTweeter/Fragment/NewTweetFragment.java
+++ src/com/DGSD/TweeterTweeter/Fragment/NewTweetFragment.java
@@ -3,31 +3,56 @@ package com.DGSD.TweeterTweeter.Fragment;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FilterQueryProvider;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
+import com.DGSD.TweeterTweeter.Data.Database;
+import com.DGSD.TweeterTweeter.Data.FollowersProvider;
+import com.DGSD.TweeterTweeter.Data.FollowingProvider;
 import com.DGSD.TweeterTweeter.R;
+import com.DGSD.TweeterTweeter.Utils.MentionsTokenizer;
 import com.DGSD.TweeterTweeter.Utils.Utils;
+import com.github.droidfu.widgets.WebImageView;
 
 /**
  * Author: Daniel Grech
  * Date: 26/11/11 6:03 PM
  * Description :
  */
-public class NewTweetFragment extends DialogFragment {
+public class NewTweetFragment extends DialogFragment  implements LoaderManager.LoaderCallbacks<Cursor>, SimpleCursorAdapter.ViewBinder {
     private static final String TAG = NewTweetFragment.class.getSimpleName();
+
+    protected static final String[] FROM = { Database.Field.SCREEN_NAME, Database.Field.IMG };
+
+    protected static final int[] TO = { R.id.name, R.id.image };
+
+    protected static String[] ROWS_TO_RETURN = {
+            Database.Field.SCREEN_NAME,
+            Database.Field.IMG,
+            Database.Field.ID
+    };
+
+    protected static class CursorCols {
+        public static int img = -1;
+    }
 
     public static final int MAX_TWEET_SIZE = 140;
 
@@ -42,6 +67,10 @@ public class NewTweetFragment extends DialogFragment {
     private MenuItem mSubmitAction;
 
     private MultiAutoCompleteTextView mTweetText;
+
+    private SimpleCursorAdapter mAdapter;
+
+    private String mCurrentFilter;
 
     public static NewTweetFragment newInstance() {
         final NewTweetFragment f = new NewTweetFragment();
@@ -85,7 +114,10 @@ public class NewTweetFragment extends DialogFragment {
             }
         }
 
-        //setupMentionCompletion();
+        setupMentionCompletion();
+
+        // Prepare the loader.  Either re-connect with an existing one, or start a new one.
+        getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
@@ -222,6 +254,93 @@ public class NewTweetFragment extends DialogFragment {
         else {
             tv.append(" " + text);
         }
+    }
+
+    /**
+     * Setup for completion of '@' mentions
+     */
+    private void setupMentionCompletion() {
+        mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item_people, null, FROM, TO, 0);
+
+        mAdapter.setViewBinder(this);
+
+        mAdapter.setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
+            @Override
+            public CharSequence convertToString(Cursor cursor) {
+                return new StringBuilder().append('@')
+                                          .append(cursor.getString(cursor.getColumnIndex(Database.Field.SCREEN_NAME)))
+                                          .toString();
+            }
+        });
+
+        //Filter as we type
+        mAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+
+            @Override
+            public Cursor runQuery(CharSequence constraint) {
+                if(constraint != null) {
+                    mCurrentFilter = new StringBuilder().append("\"@\" ||")
+                                                        .append(Database.Ordering.SCREEN_NAME)
+                                                        .append(" LIKE \"")
+                                                        .append(constraint)
+                                                        .append("%\"")
+                                                        .toString();
+                    getLoaderManager().restartLoader(0, null, NewTweetFragment.this);
+                    mCurrentFilter = null;
+                }
+
+                return mAdapter.getCursor();
+            }
+        });
+
+
+        mTweetText.setAdapter(mAdapter);
+
+        mTweetText.setThreshold(1);
+
+        mTweetText.setTokenizer(new MentionsTokenizer());
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        //Get a cursor for all entry items
+        return new CursorLoader(getActivity(), FollowingProvider.CONTENT_URI,
+                ROWS_TO_RETURN, mCurrentFilter, null,
+                Database.Ordering.SCREEN_NAME + Database.Ordering.ASC);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Swap the new cursor in. (Old cursor is automatically closed)
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
+        mAdapter.swapCursor(null);
+    }
+
+    @Override
+    public boolean setViewValue(View view, Cursor cursor, int col) {
+        if(CursorCols.img < 0) {
+            //We need to get the column numbers
+            CursorCols.img = cursor.getColumnIndex(Database.Field.IMG);
+        }
+
+        if(col == CursorCols.img) {
+            //Only load the image if we are not currently flinging the listview
+            WebImageView img = (WebImageView) view;
+            //img.reset();
+            img.setImageUrl(cursor.getString(CursorCols.img));
+            img.loadImage();
+
+            return true;
+        }
+
+        return false;
     }
 
     private class TweetLengthWatcher implements TextWatcher {
